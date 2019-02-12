cfd49cf5e18a359a01e2679bc402a85ba5454b1d
Harry Chang <harrychang1017@gmail.com>
2019-02-12 14:05:32 +0800

    Create d236.py

---
+++ Other/d236.py
@@ -0,0 +1,16 @@
+import sys
+
+'''
+res = False
+for a in range(1, 1000):
+    if res == True:
+        break
+    for b in range(a, 1000):
+        c = 1000 - a - b
+        if a ** 2 + b ** 2 == c ** 2:
+            print('%d %d %d' % (a, b, c))
+            res = True
+            break
+'''
+# (a, b, c) = (200, 375, 425)
+print(31875000)
